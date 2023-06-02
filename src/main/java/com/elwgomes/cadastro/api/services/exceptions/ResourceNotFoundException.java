fc21f86950cc43595d8b76be470b2541affeee4b
Leonado Root <elwgomes@icloud.com>
2023-06-02 11:03:08 -0300

    handling exception errors

---
+++ src/main/java/com/elwgomes/cadastro/api/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.elwgomes.cadastro.api.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+            super("Resource not found. Id " + id);
+    }
+}
